09b8332ace0ceebb6e09c52790e7324758ba9fc0
Anteneh Tessema <antenehdatascienceandml@gmail.com>
2024-10-30 22:30:44 -0400

    Initial Commit

---
+++ voiceNotes/voiceNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceNotesApp.swift
+//  voiceNotes
+//
+//  Created by anteneh tessema on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
